36e33eef3e31c447c3179116bdb081387c510bf2
Mikael Lindberg <Mik.Lindberg@outlook.com>
2021-08-14 01:21:31 +0200

    Add appointment calendar and modal func.

--- AppointmentScheduler/wwwroot/js/script-custom-calendar.js
+++ AppointmentScheduler/wwwroot/js/script-custom-calendar.js
@@ -1,1 +1,28 @@
-﻿
+﻿$(document).ready(function () {
+    InitializeCalendar();
+});
+
+function InitializeCalendar() {
+    try {
+        $('#calendar').fullCalendar({
+            timezone: false,
+            header: {
+                left: 'prev,next,today',
+                center: 'title',
+                right: 'month, agendaWeek, agendaDay'
+            },
+            selectable: true,
+            editable: false,
+            select: function (event) {
+                onShowModal(event, null);
+            }
+        });
+    }
+    catch (e) {
+        alert(e);
+    }
+}
+
+function onShowModal(obj, isEventDetail) {
+    $('#appointmentInput').modal('show');
+}
